f4c3fa47fa8a757f3e025b3edda33fe7420477d9
User <Loclhse172459@fpt.edu.vn>
2025-08-29 13:32:14 +0700

    done

--- src/main/java/com/josh/foodorder/config/WebConfig.java
+++ src/main/java/com/josh/foodorder/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","http://103.90.227.18","http://localhost:3000")
+                        .allowedOrigins("http://localhost:5173","http://103.90.227.18","http://localhost:3000","http://103.90.227.18:8081")
                         .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
